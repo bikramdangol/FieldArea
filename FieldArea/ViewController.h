aca3dbd0568ace5a59f4063ba855f84ed3aa23c1
Bikram Dangol <Bikram.Dangol@davita.com>
2015-11-20 18:33:07 -0700

    Initial Commit

---
+++ FieldArea/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  FieldArea
+//
+//  Created by Bikram Dangol on 11/20/15.
+//  Copyright © 2015 AppCoders. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ FieldArea/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  FieldArea
+//
+//  Created by Bikram Dangol on 11/20/15.
+//  Copyright © 2015 AppCoders. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+        @autoreleasepool {
+            return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+        }
+}
