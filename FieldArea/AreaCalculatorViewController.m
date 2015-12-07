cd7e2950e10ae27fe875a25c7f0f2995480f1889
Bikram Dangol <Bikram.Dangol@davita.com>
2015-12-06 21:31:59 -0700

    Added navigation bar and bar button items on it in Area Calculator screen.

--- FieldArea/AreaCalculatorViewController.m
+++ FieldArea/AreaCalculatorViewController.m
@@ -14,6 +14,10 @@
 
 @implementation AreaCalculatorViewController
 
+- (IBAction)backButtonPressed:(UIBarButtonItem *)sender {
+        [self.navigationController popViewControllerAnimated:YES]; 
+}
+
 - (void)viewDidLoad {
     [super viewDidLoad];
     // Do any additional setup after loading the view.
